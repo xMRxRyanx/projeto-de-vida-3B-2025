bf3ee2cf922b4b29c1b6aee99b1f4a432b11aec1
xMRxRyanx <161638993+xMRxRyanx@users.noreply.github.com>
2025-05-06 16:24:10 -0300

    06/05/25

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 const botoes = document.querySelectorAll(".botao");
 console.log(botoes);
+
 for (let i = 0; i < botoes.length; i++) {
+
+    for (let j = 0; j < botoes.length; j++) {
+        botoes[j].classList.remove("ativo");
+    }
 
     botoes[i].onclick = function () {
         botoes[i].classList.add("ativo");
     }
-    console.log(1);
+
 }
