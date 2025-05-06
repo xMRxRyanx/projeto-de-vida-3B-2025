25ef2f86bd40ef38fdefb2005e76ab25a1b379cb
xMRxRyanx <161638993+xMRxRyanx@users.noreply.github.com>
2025-05-06 16:13:16 -0300

    06/05/25

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
 const botoes = document.querySelectorAll(".botao");
 console.log(botoes);
+for (let i = 0; i < botoes.length; i++) {
+
+    botoes[i].onclick = function () {
+        botoes[i].classList.add("ativo");
+    }
+    console.log(1);
+}
