a20f122ba01d90955c14f20364f5832ee0e9448a
xMRxRyanx <161638993+xMRxRyanx@users.noreply.github.com>
2025-05-13 15:30:58 -0300

    13/05

--- main.js
+++ main.js
@@ -3,11 +3,10 @@ console.log(botoes);
 
 for (let i = 0; i < botoes.length; i++) {
 
-    for (let j = 0; j < botoes.length; j++) {
-        botoes[j].classList.remove("ativo");
-    }
-
     botoes[i].onclick = function () {
+        for (let j = 0; j < botoes.length; j++) {
+            botoes[j].classList.remove("ativo");
+        }
         botoes[i].classList.add("ativo");
     }
 
