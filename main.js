f865be8eb1ee6f00a3dbf17ca8b572504d63659b
xMRxRyanx <161638993+xMRxRyanx@users.noreply.github.com>
2025-06-11 11:12:11 -0300

    11/06

--- main.js
+++ main.js
@@ -16,9 +16,18 @@ for (let i = 0; i < botoes.length; i++) {
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
-let tempoAtual = new Date();
+const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
+const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
+const tempoObjetivo4 = new Date("2023-01-05T00:00:00");
+const tempo=[tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
-contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+function atualizaCronometro() {
+ for(let i=0; i< contadores.length; i++) {
+contadores[i].textContent = calculaTempo(tempos[i]);
+}
+   
+}
+
 
 function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
