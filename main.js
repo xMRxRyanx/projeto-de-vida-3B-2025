909068ee55e2939daca333656a564600794625dd
xMRxRyanx <161638993+xMRxRyanx@users.noreply.github.com>
2025-07-30 11:39:53 -0300

    30/07/25

--- main.js
+++ main.js
@@ -36,8 +36,12 @@ function calculaTempo(tempoObjetivo) {
     minutos %= 60;
     horas %= 24;
 
-    return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+    if(tempoFinal>0){
+      return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+    }
+    else{return[0,0,0,0];}
 }
+
 function atualizaCronometro() {
     for (let i = 0; i < contadores.length; i++) {
         contadores[i].textContent = calculaTempo(tempos[i]);
