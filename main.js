c2306be59d060d6d4ca4c77fdb2aae1ebc09935f
xMRxRyanx <161638993+xMRxRyanx@users.noreply.github.com>
2025-08-06 11:13:51 -0300

    06/08

--- main.js
+++ main.js
@@ -49,7 +49,7 @@ function atualizaCronometro() {
     document.getElementById("dias0").textContent = calculaTempo(tempos[0])[3];
     for (let i = 0; i < contadores.length; i++) {
         contadores[i].textContent = calculaTempo(tempos[i]);
-        document.getElementById("seg"+i).textContent = calculaTempo(tempos[1])[3]; 
+        document.getElementById("seg"+i).textContent = calculaTempo(tempos[i])[3]; 
         console.log("seg"+i);
         
     }
